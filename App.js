a888b9dbcf33b86b65cecbd1db30cf040121037b
Piotr Błażejak <piotr.blazejak@yahoo.com>
2022-05-22 18:05:08 +0200

    task input

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+
 import Task from "./compontents/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.wrapper}>
+      <View style={styles.taskWrapper}>
         <Text style={styles.title}>My tasks</Text>
         <View style={styles.list}>
           <Task text="this is task no 1" />
           <Task text="this is task no 2" />
         </View>
       </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.addTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={"add new task"} />
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -19,10 +38,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#DDD",
-  },
-  wrapper: {
-    paddingTop: 20,
     paddingHorizontal: 20,
+  },
+  taskWrapper: {
+    paddingTop: 20,
   },
   title: {
     fontSize: 24,
@@ -32,4 +51,31 @@ const styles = StyleSheet.create({
   list: {
     marginTop: 30,
   },
+  addTaskWrapper: {
+    position: "absolute",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    bottom: 60,
+    width: "100%",
+  },
+  input: {
+    padding: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 50,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 50,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addText: {},
 });
